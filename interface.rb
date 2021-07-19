3adb0875fb7913cb6971b1d56bbc40d6edfbb156
Roberto Barros <roberto.f.barros@gmail.com>
2021-07-19 11:57:25 -0300

    pseudocode

---
+++ interface.rb
@@ -0,0 +1,17 @@
+# Boas vindas ao nosso jogo dos cavalinhos
+
+# criar os cavalinhos
+
+# Mostrar o nome dos cavalinhos
+
+# Perguntar em qual cavalinho ele vai apostar
+
+# Pegar o cavalinho da aposta
+
+# Rodar a corrida
+
+# Mostrar quem é o vencedor
+
+# Mostrar se ele ganhou ou perdeu
+
+
